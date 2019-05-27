1946cbb8f15507c1014a73252db75eaf2c81a415
Iskan <andrea.belkhir@yahoo.it>
2019-05-27 12:17:47 +0200

    add adminform.class/helpadmin/logincontroller

---
+++ progetto_siw/src/controller/HelperAdmin.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HelperAdmin {
+
+	public boolean IsNotValid(HttpServletRequest request) throws UnsupportedEncodingException {
+
+		String id = request.getParameter("id");  
+		String password = request.getParameter("cognome");
+
+		boolean errore = false;
+
+		if(id.isEmpty()) {
+			String  mess = "Il nome e' un campo obbligatorio";
+			request.setAttribute("idErr", mess );  //Passo alla form il messaggio di errore
+			errore = true;
+		}
+
+		if(password.isEmpty()) {
+			String  mess = "Il cognome e' un campo obbligatorio";
+			request.setAttribute("passwordErr", mess );  //Passo alla form il messaggio di errore
+			errore = true;
+		}
+		if(&&!errore)//TODO controlla se password è associata al id presente nella lista di admin nel server 
+			{
+			String mess="l'associazione id password e' errata";
+			request.setAttribute("idpasErr", mess);
+			errore= true;
+		}
+
+		return errore;
+	}
+}
---
+++ progetto_siw/src/controller/LogInController.java
@@ -0,0 +1,49 @@
+package controller;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Admin;
+import services.Adminservices;
+
+@WebServlet("/LogIn")
+public class LogInController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//Lettura dei parametri
+
+		HelperAdmin helperstudente = new HelperAdmin();
+		AdminForm adminForm = new AdminForm();
+		HttpSession session = req.getSession();
+		//Gestione della risposta
+		String nextPage;
+
+		if(helperstudente.IsNotValid(req)) {
+			nextPage = "/ControlloAdim.jsp";
+
+		}
+		else {
+			adminForm.setId(req.getParameter(id));
+			adminForm.setPassword(req.getParameter("password"));
+			session.setAttribute("AdminForm", adminForm);
+			nextPage = "/risposta.jsp";
+
+		}
+		ServletContext application = getServletContext();
+		RequestDispatcher rd;
+		rd = application.getRequestDispatcher(nextPage);
+		rd.forward(req, resp);
+		return;
+
+	}
+	}
+
+}
