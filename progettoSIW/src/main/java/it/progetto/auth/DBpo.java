87f3a02439ebb653839064847e10675f2764bb1f
Iskan <andrea.belkhir@yahoo.it>
2019-06-17 10:27:45 +0200

    messo auth per admin e i loggin

---
+++ progettoSIW/src/main/java/it/progetto/auth/DBpo.java
@@ -0,0 +1,29 @@
+package it.progetto.auth;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import it.progetto.model.Admin;
+import it.progetto.repository.AdminRepository;
+
+public class DBpo implements ApplicationRunner {
+@Autowired
+private AdminRepository admin;
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+this.population();
+	}
+private void population() throws IOException, InterruptedException{
+	if(admin.count()==0) {
+    Admin admin = new Admin("admin", null, "ADMIN");
+    String adminPassword = new BCryptPasswordEncoder().encode("admin");
+    admin.setPassword(adminPassword);
+    admin = this.admin.save(admin);  
+	}
+}
+}
---
+++ progettoSIW/src/main/java/it/progetto/controller/MainController.java
@@ -0,0 +1,33 @@
+package it.progetto.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class MainController {
+	  public MainController() {
+	        super();
+	    }
+	    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	    public String index(Model model) {
+	        return "index.html";
+	    }
+
+
+	    @RequestMapping(value = { "/admin" }, method = RequestMethod.GET)
+	    public String admin(Model model) {
+	        UserDetails details = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	        String role = details.getAuthorities().iterator().next().getAuthority();    // get first authority
+	        model.addAttribute("username", details.getUsername());
+	        model.addAttribute("role", role);
+
+	        return "paginaAdmin";
+	    }
+
+}
